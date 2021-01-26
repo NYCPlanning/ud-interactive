25dbcdb97933099acc68a37e7ff6907ff34696e1
Carsten Rodin <crodin@planning.nyc.gov>
2021-01-26 14:09:30 -0500

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `UD Interactive`,
+    description: `Demo site for Urban Design interactive drawings and models.`,
+    author: `Carsten Rodin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
