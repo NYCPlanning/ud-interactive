c6f3fc6519b36957446f10aeb91744b1f01bf71c
Carsten Rodin <crodin@planning.nyc.gov>
2021-02-16 16:49:02 -0500

    adds example scene loading from triceratops/grasshopper

---
+++ src/pages/triceratops-export.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Blank from '../layouts/blank'
+import Triceratops from '../scenes/triceratops-export/scene'
+
+export default () => (
+  <Blank>
+    <Triceratops />
+  </Blank>
+)
