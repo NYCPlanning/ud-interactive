96f097d8c3ed1e59b1eb454b6c46a5c1b26d333c
Carsten Rodin <crodin@planning.nyc.gov>
2021-03-03 11:04:22 -0500

    adds index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,56 @@ import { Link } from 'gatsby'
 import SEO from '../components/seo'
 import Layout from '../layouts/default'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title='Home' />
-    <div className='mt-8 p-4'>
-      <h2>Demos</h2>
-      <ul>
-        <li>
-          <Link to='/residential-buildings'>
-            Residential Buildings
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </Layout>
-)
+export const query = graphql`
+  query AllPages {
+    allSitePage {
+      edges {
+        node {
+          path
+        }
+      }
+    }
+  }
+`
+
+const excludePages = [
+  '/dev-404-page/',
+  '/404/',
+  '/',
+  '/404.html',
+]
+
+const PageLink = ({path}) => {
+  return (
+    <li>
+      <Link to={path}>
+        {path}
+      </Link>
+    </li>
+  )
+}
+
+const IndexPage = ({data}) => {
+  const { allSitePage: { edges: pages }} = data
+
+  const pageLinks = pages.filter(({ node: { path }}) => (
+      !excludePages.includes(path)
+    )).map((p, i) => (
+      <PageLink path={p.node.path} key={i} />
+    ))
+
+  return (
+    <Layout>
+      <SEO title='Home' />
+      <div className='mt-8 p-4'>
+        <h2>Demos</h2>
+        <ul>
+          {pageLinks}
+        </ul>
+      </div>
+    </Layout>
+  )
+}
+
 
 export default IndexPage
