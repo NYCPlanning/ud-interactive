fa51847a8070ee01cb7a77871eb9fba5c5e7c893
Carsten Rodin <crodin@planning.nyc.gov>
2021-03-03 10:44:50 -0500

    adds stub for realtime demonstration

---
+++ src/pages/realtime-test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Blank from '../layouts/blank'
+// import Triceratops from '../scenes/triceratops-export/scene'
+
+export default () => (
+  <Blank>
+    REALTIME TEST
+  </Blank>
+)
