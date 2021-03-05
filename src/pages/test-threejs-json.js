d15efd9628b6b2b42e7a91db1a581e229a8eb885
Carsten Rodin <crodin@planning.nyc.gov>
2021-03-05 18:52:10 -0500

    adds export checker for three js objects from occ-core

---
+++ src/pages/test-threejs-json.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Blank from '../layouts/blank'
+import Preview from '../scenes/occ-export/scene'
+
+export default () => (
+  <Blank>
+    <Preview />
+  </Blank>
+)
