5f280bcd3cebb900cef326994728cb5289cb3bb4
Carsten Rodin <crodin@planning.nyc.gov>
2021-03-15 19:54:12 -0400

    removes realtime and grpc tests for deploy

--- src/pages/udtools-rpc-test.js
+++ src/pages/udtools-rpc-test.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import wkx from 'wkx'
-import { Buffer } from 'buffer'
-import { client, TaxLot, Site } from '../sources/udtools'
+// import wkx from 'wkx'
+// import { Buffer } from 'buffer'
+// import { client, TaxLot, Site } from '../sources/udtools'
 import Blank from '../layouts/blank'
 
 export default () => {
   // create a TaxLot object for the request
-  var request = new TaxLot({bbl: '1001230002'})
+  // var request = new TaxLot({bbl: '1001230002'})
 
   // make a remote procedure call to get a Site object
-  client.makeSite(request, {}, (err, response) => {
-    console.log(response.getId())
-    console.log(response)
-    const geomWkb = new Buffer.from(response.getGeom())
-    const geom = wkx.Geometry.parse(geomWkb)
-    console.log(geom)
-  })
+  // client.makeSite(request, {}, (err, response) => {
+  //   console.log(response.getId())
+  //   console.log(response)
+  //   const geomWkb = new Buffer.from(response.getGeom())
+  //   const geom = wkx.Geometry.parse(geomWkb)
+  //   console.log(geom)
+  // })
 
   return (
     <Blank>
