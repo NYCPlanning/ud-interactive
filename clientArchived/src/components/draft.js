e9ac42cb93ee20d5d9f8d4082e5f75b78cf4ee10
Carsten Rodin <crodin@planning.nyc.gov>
2021-06-21 13:28:28 -0400

    deletes archived web client, to be parceled out and re-added as separate clients

--- clientArchived/src/components/draft.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Draft() {
-  return (
-    <span className='p-4 bg-white text-red-500 font-bold text-xl'>
-      DRAFT
-    </span>
-  )
-}
--- clientArchived/src/sources/udtools.js
+++
@@ -1,8 +0,0 @@
-// const { Site } = require('./lib/site_pb.js');
-// const { TaxLot } = require('./lib/model_base_pb.js');
-// const { SiteServiceClient } = require('./lib/site_grpc_web_pb.js');
-
-// //var client = new SiteServiceClient('http://0.0.0.0:8088');
-// var client = new SiteServiceClient(`http://${process.env.REACT_APP_UDTOOLS_URI}`);
-
-// export { client, Site, TaxLot }
