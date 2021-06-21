a9caece75e743c0d3cb5bfdfd30a1239b4ba2e7c
Lucas Gelfond <lucas_gelfond@brown.edu>
2021-06-21 16:13:23 -0400

    svg landing page

---
+++ svg-client-basic/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CityBlocks from './assets/city-blocks.svg'
+
+function App() {
+  return (
+    <div className="App">
+
+      <img src={CityBlocks}></img>
+    </div>
+  );
+}
+
+export default App;
