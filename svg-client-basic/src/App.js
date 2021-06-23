a5dbf70ecb3cca80d9514745909f5f18b8026283
Lucas Gelfond <lucas_gelfond@brown.edu>
2021-06-23 10:14:03 -0400

    Linting/style
    pretty sure I got Prettier/ESLint working with the AirBNB style guide, although I guess only time will tell—the videos I watched gave fairly mixed instructions so I think this will probably be a good jumping off point but one that I will need to reconfigure/play around with as I go. Only thing I really couldn't figure out was how to get the code formatter to work with "npm run lint" or "npm run lint:fix" by adding scripts to package.json, I tried to debug this for a bit (per StackOverflow, clearing cache, deleting node_modules, reinstalling node packages) but it just didn't seem worth the effort when "eslint . --fix" and "eslint ." both will do the job with about the same amount of writing as the npm commands. Once again lightly worried I installed some of this stuff wrong but it seems mostly good. Need to still fix the formatting to actually get it to the AirBNB style, however.

--- svg-client-basic/src/App.js
+++ svg-client-basic/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import CityBlocks from './assets/city-blocks.svg'
+import CityBlocks from './assets/city-blocks.svg';
 
 function App() {
   return (
     <div className="App">
 
-      <img src={CityBlocks}></img>
+      <img src={CityBlocks} />
     </div>
   );
 }
