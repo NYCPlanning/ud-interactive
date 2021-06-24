92ee8f308e8b8569e10f605519c82c13fe1baace
Lucas Gelfond <lucas_gelfond@brown.edu>
2021-06-24 18:39:54 -0400

    orbit controls / orbiting + also some ESLint fixes

--- svg-client-basic/src/components/Testing.jsx
+++ svg-client-basic/src/components/Testing.jsx
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
-import { Canvas } from "react-three-fiber";
+import React, { useState, useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Canvas, extend, useThree, useFrame } from "react-three-fiber";
 import { boxBufferGeometry } from "three";
 import "../assets/style.css";
 import { a, useSpring } from "@react-spring/three";
+
+extend({ OrbitControls });
+
+const Controls = () => {
+  const orbitRef = useRef();
+  const { camera, gl } = useThree();
+  useFrame(() => {
+    orbitRef.current.update();
+  });
+  return (
+    <orbitControls
+      maxPolarAngle={Math.PI / 3}
+      minPolarAngle={Math.PI / 3}
+      autoRotate
+      args={[camera, gl.domElement]}
+      ref={orbitRef}
+    />
+  );
+};
 
 const Box = () => {
   const [hovered, setHovered] = useState(false);
@@ -28,6 +48,7 @@ export default function Testing() {
   return (
     <div>
       <Canvas>
+        <Controls />
         <Box />
       </Canvas>
     </div>
