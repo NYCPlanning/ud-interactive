664cc5f62ba7d84423541c19266a53a434f4e2d4
Lucas Gelfond <lucas_gelfond@brown.edu>
2021-06-29 12:46:08 -0400

    tooltips on svg + eslint/prettier fixes!
    should've separated these into two commits but forgot—now have tooltips on the original SVG drawing!

--- svg-client-basic/src/App.jsx
+++ svg-client-basic/src/App.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Tooltip } from 'react-svg-tooltip';
 import './App.css';
-import CityBlocks from './assets/city-blocks.svg';
+import { ReactComponent as CityBlocks } from './assets/city-blocks.svg';
 
 function App() {
+  const circleRef1 = useRef(null);
+
   return (
     <div className="App">
-      <img alt="NYC city blocks" src={CityBlocks} />
+      <svg viewBox="0 0 100 100">
+        <CityBlocks />
+
+        <circle ref={circleRef1} cx={72} cy={30} r={1} fill="red" />
+        <Tooltip triggerRef={circleRef1}>
+          <rect x={2} y={2} width={20} height={5} rx={0.5} ry={0.5} fill="black" />
+          <text x={5} y={5} fontSize={2} fill="white">
+            Building label
+          </text>
+        </Tooltip>
+
+        <circle ref={circleRef1} cx={53} cy={44} r={1} fill="red" />
+        <Tooltip triggerRef={circleRef1}>
+          <rect x={2} y={2} width={15} height={5} rx={0.5} ry={0.5} fill="black" />
+          <text x={5} y={5} fontSize={2} fill="white">
+            Park label
+          </text>
+        </Tooltip>
+
+        <circle ref={circleRef1} cx={35} cy={58} r={1} fill="red" />
+        <Tooltip triggerRef={circleRef1}>
+          <rect x={2} y={2} width={15} height={5} rx={0.5} ry={0.5} fill="black" />
+          <text x={5} y={5} fontSize={2} fill="white">
+            Street label
+          </text>
+        </Tooltip>
+      </svg>
     </div>
   );
 }
