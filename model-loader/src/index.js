002eefd57085779299e13e015c08b018748d22bd
Lucas Gelfond <lucas_gelfond@brown.edu>
2021-07-07 14:38:20 -0400

    buggy showing up!

--- model-loader/src/index.js
+++ model-loader/src/index.js
@@ -4,12 +4,18 @@ import { Canvas } from "react-three-fiber";
 import { OrbitControls, Stats } from "@react-three/drei";
 
 import Plane from "./model";
+import Model from './DOC';
+import Plane2 from './Small-airplane-v3';
+import Buggy from './Buggy';
 
 const App = () => (
   <Canvas style={{ height: 400, width: 800 }}>
     <pointLight position={[5, 5, 5]} />
     <Suspense fallback={null}>
-      <Plane rotation={[0, Math.PI * 1.25, 0]} />
+      {/* <Plane rotation={[0, Math.PI * 1.25, 0]} /> */}gl
+      {/* <Model /> */}
+      {/* <Plane2 /> */}
+      <Buggy />
     </Suspense>
     <OrbitControls />
     {/* <Stats /> */}
