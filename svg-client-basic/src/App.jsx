0bd330a0df697c30a512c60c7d0e893727c1cb5e
Lucas Gelfond <lucas_gelfond@brown.edu>
2021-07-23 13:51:07 -0400

    built captions

--- svg-client-basic/src/App.jsx
+++ svg-client-basic/src/App.jsx
@@ -4,6 +4,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import AnimatedScene from './components/AnimatedScene';
 import Previous from './components/Previous';
 import Next from './components/Next';
+import Caption from './components/Caption';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(props) {
@@ -28,8 +29,15 @@ function App(props) {
               saveAnimationTime={saveAnimationTime}
               inReverse={inReverse}
             />
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={5} xs={5} md={5} lg={5}>
             <Previous previousPos={previousPos} />
             <Next nextPos={nextPos} />
+          </Col>
+          <Col sm={5} xs={5} md={5} lg={5}>
+            <Caption posNumber={posNumber} />
           </Col>
         </Row>
       </Container>
