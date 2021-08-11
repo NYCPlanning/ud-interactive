1447cfed361a478d07bfac945b09cac9d4b23cf8
Lucas Gelfond <lucas_gelfond@brown.edu>
2021-08-11 13:46:26 -0700

    navigation bar finished

--- svg-client-basic/src/components/Navigation/index.jsx
+++ svg-client-basic/src/components/Navigation/index.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Dropdown, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import information from './subcomponents/ViewControl/assets/information.svg';
 
-import ViewsDropdown from './subcomponents/ViewsDropdown';
+import ViewControl from './subcomponents/ViewControl';
 
 export default function Navigation() {
   return (
-    <div id="toolbar">
-      <Button id="next" className="buttonElement" type="button">
+    <div id="toolbar" className="d-inline-block navigation">
+      <Dropdown className="noSelect buttonElement d-inline-block bar-padding shadow-none">
+        <Dropdown.Toggle
+          className="div-inline dropdown buttonElement noSelect shadow-none"
+          variant="success"
+          id="dropdown-basic"
+        >
+          Residential View
+        </Dropdown.Toggle>
+        <Dropdown.Menu className="dropdown " id="menu">
+          <Dropdown.Item className="dropdown-item" href="#/action-1">
+            Waterfront View
+          </Dropdown.Item>
+          <Dropdown.Item className="dropdown-item" href="#/action-2">
+            Industrial View
+          </Dropdown.Item>
+          <Dropdown.Item className="dropdown-item" href="#/action-3">
+            Park View
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <Button
+        id="next"
+        className="buttonElement d-inline-block bar-padding noSelect shadow-none"
+        type="button"
+      >
         Next
       </Button>
-      <ViewsDropdown />
+      <div id="control-group" className="d-inline-block">
+        <ViewControl control="left" className="d-inline-block view-control" />
+        <ViewControl control="right" className="d-inline-block view-control" />
+        <ViewControl control="up" className="d-inline-block view-control" />
+        <ViewControl control="down" className="d-inline-block view-control" />
+      </div>
+      <h1 id="streetscapes-text">STREETSCAPES FOR WELLNESS</h1>
+      <img id="information" className="d-inline-block" src={information} alt="information" />
     </div>
   );
 }
