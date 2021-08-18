fe1d69a9b4bab9459a8000932eb38c4f2182cac1
Carsten Rodin <crodin@planning.nyc.gov>
2021-08-18 14:22:27 -0400

    cleanup commented lines

--- client-streetscapes-for-wellness/src/App.jsx
+++ client-streetscapes-for-wellness/src/App.jsx
@@ -4,11 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AnimatedScene from './components/AnimatedScene';
 import Navigation from './components/Navigation/index';
 
-// const camPositions = camPositionsCalc();
-
 function App() {
-  // const testObj = { currAnimStartTime, currAnimStartPos, currAnimEndTime, currAnimEndPos };
-  // console.log(JSON.stringify(testObj));
   return (
     <div className="App">
       <AnimatedScene />
