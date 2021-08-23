ef0df787e1f93d9d7d673a24d5eba5eeb13e5404
Carsten Rodin <crodin@planning.nyc.gov>
2021-08-23 17:30:55 -0400

    wip FromGLTF with cameras and original scene to state ref

--- client-streetscapes-for-wellness/src/components/FromGLTF.jsx
+++ client-streetscapes-for-wellness/src/components/FromGLTF.jsx
@@ -1,23 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { useGLTF } from '@react-three/drei';
-import { ref } from 'valtio';
+import { meshBounds, useGLTF } from '@react-three/drei';
+import * as THREE from 'three';
+import { ref, useSnapshot } from 'valtio';
 
 import { state } from '../state';
 
+const neutralColor = new THREE.Color(1, 1, 1);
+const highlightColor = new THREE.Color(0xe0a254);
+
 
 const FromGLTF = ({ src }) => {
-  const { scene, cameras } = useGLTF(src);
+  let { scene, cameras } = useGLTF(src);
+  const { dematerialize } = useSnapshot(state);
+  let origMaterials = [];
 
   // pass loaded cameras to state for navigation
   state.cameras = ref(cameras)
+  state.scene = ref(scene)
 
-  // traverse the scene and adjust settings
+  // apply shadows
   scene.traverse((o) => {
-    if ( o.type === 'Mesh' && o.name === 'buildings') {
+    if ( o.name === 'buildings' || o.name === 'ekevated' || o.name === 'bench' ) {
+      // console.log(o)
       o.castShadow = true;
     }
+
+    if ( o.name === 'sidewalk' || o.name === 'hardscape' ) {
+      // console.log(o)
+      o.receiveShadow = true;
+    }
+
+    //note original materials
+    if ( o.type === 'Mesh') {
+      origMaterials.push({'uuid': o.uuid, 'color': o.material.color});
+    }
   });
+
+  useEffect(() => {
+    if ( dematerialize ) {
+      // console.log('dematerializing')
+      scene.traverse((o) => {
+        if ( o.type === 'Mesh' && o.material.name != 'HIGHLIGHT_COLOR') {
+          o.material.color = neutralColor;
+          o.material.needsUpdate = true
+        }
+
+        // if ( o.type === 'Mesh' &&  ) {
+        //   o.material.color = highlightColor;
+        //   o.material.needsUpdate = true
+        // }
+      })
+    }
+    // } else {
+    //   scene.traverse((o) => {
+    //     if ( o.type === 'Mesh') {
+    //       o.material.color = o.material.originalColor;
+    //       o.material.needsUpdate = true
+    //     }
+
+    //     if ( o.type === 'Mesh' && o.material.name === 'HIGHLIGHT_COLOR' ) {
+    //       o.material.color = o.material.originalColor;
+    //       console.log(o.material.originalColor)
+    //       o.material.needsUpdate = true
+    //     }
+    //   })
+    // }
+  }, [dematerialize]);
 
   return (
     <primitive
