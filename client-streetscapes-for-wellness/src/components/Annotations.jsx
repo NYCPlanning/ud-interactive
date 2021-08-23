03bb4ecd82948f8536b0504c424f56c26773975d
Carsten Rodin <crodin@planning.nyc.gov>
2021-08-23 17:23:51 -0400

    adds wip annotations

---
+++ client-streetscapes-for-wellness/src/components/Annotations.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { Html } from '@react-three/drei'
+import { useSnapshot } from 'valtio'
+
+import { state } from '../state'
+
+
+const Annotation = ({ text, ...props }) => (
+  <Html {...props} >
+    <div className='bg-white p-2'>
+      {text}
+    </div>
+  </Html>
+)
+
+
+const Annotations = () => {
+  const { scene, showAnnotations } = useSnapshot(state)
+  const annos = scene ? scene.children.filter((o) => o.name.startsWith('anno')) : []
+  const annoComponents = annos.map((a) => <Annotation position={a.position} text={a.userData.text} /> )
+  
+  return (
+    <>
+      {showAnnotations && annoComponents}
+    </>
+  )
+}
+
+export default Annotations
