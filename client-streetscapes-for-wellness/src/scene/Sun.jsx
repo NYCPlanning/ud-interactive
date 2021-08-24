01d346007af1db705c931c5951047dd2d4a12107
Carsten Rodin <crodin@planning.nyc.gov>
2021-08-24 17:56:37 -0400

    splits components dir into scene and ui dirs

---
+++ client-streetscapes-for-wellness/src/scene/Sun.jsx
@@ -0,0 +1,36 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { useSnapshot } from 'valtio';
+
+import { state } from '../state';
+
+const Sun = () => {
+  const { sunPosition: pos } = useSnapshot(state);
+  const lightRef = useRef(null)
+  const [helperCamera, setHelperCamera] = useState(null)
+
+  useEffect(() => {
+    if (lightRef.current) setHelperCamera(lightRef.current.shadow.camera)
+    console.log(helperCamera)
+  }, [lightRef])
+
+  return (
+    <>
+      {/* {helperCamera && <cameraHelper args={helperCamera} /> } */}
+      <spotLight
+        ref={lightRef}
+        position={pos}
+        intensity={1.5} 
+        castShadow
+        shadow-mapSize-height={1028}
+        shadow-mapSize-width={1028}
+        shadow-camera-near={100}
+        shadow-camera-far={200}
+        shadow-camera-top={50}
+        shadow-radius={1}
+        color={0xfffaeb}
+      />
+    </>
+  )
+}
+
+export default Sun;
