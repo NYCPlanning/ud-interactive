74e749f960abe48a7ff17f2ce287c468dc4135b3
Carsten Rodin <crodin@planning.nyc.gov>
2021-08-25 13:39:39 -0400

    update navigation layout

--- client-streetscapes-for-wellness/src/ui/Navigation.jsx
+++ client-streetscapes-for-wellness/src/ui/Navigation.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSnapshot } from 'valtio'
 
-import { nextPos, previousPos, state, getCurrentSceneName } from '../state'
+import { nextPos, previousPos, state } from '../state'
 import Caption from './Caption'
 import Debugger from './Debugger'
 import Settings from './Settings'
@@ -18,13 +18,15 @@ const Navigation = () => {
   return (
     <div id='nav-root' className='fixed bottom-0 left-0 m-2'>
       {/* <span className='info-icon'>i</span> */}
-      { showCaptions && <Caption /> }
-      <div className='bg-white p-2 flex justify-between space-x-4'>
-        <button onClick={previousPos}>Prev</button>
-        <span className='text-gray-500'>{sceneName}</span>
-        <button onClick={nextPos}>Next</button>
-        {/* optional - lookaround arrows ←→↑↓*/}
-        <span className='font-bold'>Streetscapes for Wellness</span>
+      <div className='flex flex-col items-start'>
+        { showCaptions && <Caption /> }
+        <div className='bg-white p-2 flex justify-between space-x-4'>
+          <button onClick={previousPos}>Prev</button>
+          <span className='text-gray-500'>{sceneName}</span>
+          <button onClick={nextPos}>Next</button>
+          {/* optional - lookaround arrows ←→↑↓*/}
+          <span className='font-bold'>Streetscapes for Wellness</span>
+        </div>
       </div>
       <Settings />
       { showDebugger && <Debugger /> }
