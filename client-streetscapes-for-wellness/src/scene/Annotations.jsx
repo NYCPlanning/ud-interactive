2543d92492547dbaca77df0ffff3f79f6f208f5f
Carsten Rodin <crodin@planning.nyc.gov>
2021-08-25 13:38:48 -0400

    updates captions and annotations

--- client-streetscapes-for-wellness/src/scene/Annotations.jsx
+++ client-streetscapes-for-wellness/src/scene/Annotations.jsx
@@ -1,17 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import * as THREE from 'three'
 import { Html } from '@react-three/drei'
 import { useSnapshot } from 'valtio'
 
 import { state } from '../state'
 
 
-const Annotation = ({ text, ...props }) => (
-  <Html {...props} >
-    <div className='bg-white p-2'>
-      {text}
-    </div>
-  </Html>
-)
+const Annotation = ({ text, ...props }) => {
+  const p1 = new THREE.Vector3()
+  const p0 = p1.clone()
+  p0.y = -100
+
+  const leaderLine = new THREE.BufferGeometry().setFromPoints([p0, p1])
+
+  return (
+    <group {...props}>
+      <line geometry={leaderLine} >
+        <lineBasicMaterial attach="material" color={0xffffff} linewidth={10} linecap={'round'} linejoin={'round'} />
+      </line>
+      <Html>
+        <div className='caption'>
+          {text}
+        </div>
+      </Html>
+    </group>
+  )
+}
 
 
 const Annotations = () => {
