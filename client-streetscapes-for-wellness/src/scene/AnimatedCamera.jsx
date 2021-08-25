527e2faec96bffafa073014485a53cd7ad9fbeb8
Carsten Rodin <crodin@planning.nyc.gov>
2021-08-25 10:14:34 -0400

    working camera animations with react-spring via matrix transform

--- client-streetscapes-for-wellness/src/scene/AnimatedCamera.jsx
+++ client-streetscapes-for-wellness/src/scene/AnimatedCamera.jsx
@@ -6,34 +6,55 @@ in response to changes in state.
 
 */
 
-import { useEffect } from 'react';
-import * as THREE from 'three';
-import { useFrame, useThree } from '@react-three/fiber';
-import { useSnapshot } from 'valtio';
-import { state } from '../state';
+import { useEffect, useState, useRef } from 'react'
+import * as THREE from 'three'
+import { useThree } from '@react-three/fiber'
+import { useSpring, a, config } from '@react-spring/three'
+import { useSnapshot } from 'valtio'
+
+import { state } from '../state'
 
 
-export default function Dolly() {
-  const { cameras, index } = useSnapshot(state);
-  const { camera } = useThree();
-
-  useEffect(() => {
-    if  ( cameras.length === 0 ) return null;
-    const currentCamera = cameras[index];
-
-    // apply fov, near, far params
-    camera.fov = currentCamera.fov
-    camera.near = currentCamera.near
-    camera.far = currentCamera.far
-    camera.updateProjectionMatrix();
-
-    // apply matrix transform from the current camera
-    // to the next one in the scene
-    camera.matrixAutoUpdate = false;
-    camera.matrixWorld = currentCamera.matrixWorld;
-    camera.updateMatrix();
-
-  }, [cameras, index])
-
-  return null;
+const initCamera = {
+  matrix: new THREE.Matrix4(),
+  fov: 50
 }
+
+const springConfig = config.molasses // or an object with custom params
+
+
+// instead of animating position and target, we want to animate
+// matrixWorld and fov, possibly also near and far although
+// those can be defaults
+const AnimatedCamera = () => {
+  const ref = useRef()
+  const set = useThree((threeState) => threeState.set)
+  const { cameras: exportedCameras, index } = useSnapshot(state)
+  const [ current, setCurrent ] = useState(initCamera)
+  const [ cached, setCached ] = useState(initCamera)
+  const spring = useSpring({
+    config: springConfig,
+    matrix: current.matrix,
+    from: {
+      matrix: cached.matrix,
+    },
+  })
+
+  // sets the scene default camera to this one
+  useEffect(() => { void set({ camera: ref.current }) }, [ref])
+
+  // when scene index changes, set the current camera parameters
+  // this triggers the camera animation
+  useEffect(() => {
+    if ( !exportedCameras.length ) return null
+    const nextView = exportedCameras[index]
+    const m = nextView.matrixWorld.toArray()
+    setCurrent({matrix: m})
+  }, [exportedCameras, index])
+
+  return (
+    <a.perspectiveCamera ref={ref} matrixAutoUpdate={false} matrix={spring.matrix} />
+  )
+}
+
+export default AnimatedCamera
