8adb7d85de2bbb824876022402dd91cfdb74295e
Carsten Rodin <crodin@planning.nyc.gov>
2021-08-25 10:56:17 -0400

    splits state debugger into separate ui component

--- client-streetscapes-for-wellness/src/ui/Navigation.jsx
+++ client-streetscapes-for-wellness/src/ui/Navigation.jsx
@@ -1,58 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useSnapshot } from 'valtio'
 
 import { nextPos, previousPos, state } from '../state'
 import Caption from './Caption'
+import Debugger from './Debugger'
+import Settings from './Settings'
 
 
-const StateDebugger = () => {
-  const { cameras, index } = useSnapshot(state);
-
-  return (
-    <div>
-      <div>PLACEHOLDER: </div>
-      <div>{cameras.length} cameras loaded</div>
-      <div>on {index}</div>
-    </div>
-  )
-}
-
-
-const Settings = () => {
-  const { cameras, index, dematerialize, showAnnotations } = useSnapshot(state)
-  const [ i, setI ] = useState(0)
-  // const [ demat, setDemat ] = useState(false)
-
-  const toggleAnnotations = () => {
-    state.showAnnotations = !showAnnotations
-  }
-
-  const sunPositions = [
-    [45, 100, -45],
-    [65, 100, 45],
-  ]
-
-  const toggleSunPosition = () => {
-    if ( i + 1 > sunPositions.length - 1) setI(0)
-    else setI( i + 1 )
-    state.sunPosition = sunPositions[i]
-  }
-
-  const toggleDemat = () => {
-    state.dematerialize = !dematerialize
-  }
-
-  return (
-    <>
-      <button onClick={toggleSunPosition}>Change Sun</button>
-      <button onClick={toggleDemat}>Dematerialize</button>
-      <button onClick={toggleAnnotations}>Show Annotations</button>
-    </>
-  )
-}
-
-
-const AltNavigation = () => {
+const Navigation = () => {
   return (
     <div id='nav-root' className=' bg-white'>
       <div>
@@ -66,11 +21,11 @@ const AltNavigation = () => {
         <span className='title'>Streetscapes for Wellness</span>
         <span className='info-icon'>i</span>
       </div>
-      <div>
-        <Settings />
-      </div>
+      <Settings />
+      {/* <Debugger /> */}
     </div>
   )
 }
 
-export default AltNavigation
+
+export default Navigation
